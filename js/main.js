5e768a1507f86007f4451e56d775a9a4a837b1b4
Albert <lorenzdiz@gmail.com>
2017-01-28 09:49:13 +0800

    Removed template excess files Implement snake game basics - moving snake - food and food respawn

--- js/main.js
+++ js/main.js
@@ -4,96 +4,184 @@ window.onload = function() {
 	math = game.math;
 }
 
+// Game properties
 var game;
-var math;
+var graphics;
+var CELL_SIZE = 8;
+var GAME_WIDTH = 100;
+var GAME_HEIGHT = 75;
+
+// Game States
+var gameState;
+var STATE_MENU = 0;
+var STATE_PLAY = 1;
+var STATE_SETTINGS = 2;
+var STATE_PAUSED = 3;
+
+// Game entities
+var ENTITY_FOOD = "F";
+var ENTITY_ITEMS = 2;
 
 // Game inputs
 var input;
-var buttonShoot;
-var clickLeft;
-var clickRight;
 
+// Player and player project properties and methods
+var player;
+var playerX;
+var playerY;
+
+// Player properties
+var score = 0;
+var health = 10;
+var playerSpeed = 250;
+var playerAcceleration = 100;
+
+var playerDirection = 0;
+var DIRECTION_UP = 0;
+var DIRECTION_DOWN = 1;
+var DIRECTION_LEFT = 2;
+var DIRECTION_RIGHT = 3;
+
+var updateDelay;
+var grid;
 
 function preload () {
-	// player
-	game.load.image('player', 'res/player/player.png');
-	game.load.spritesheet('player_projectile', 'res/player/player_projectile.png', 32, 32);
-
-	// enemies
-	game.load.image('enemy_ship_1', 'res/enemies/grunts/enemy_ship_1.png');
-	
-	// enemy bullets
-	game.load.image('bullet_round_red', 'res/projectiles/bullet_round_red.png');
-	game.load.image('bullet_round_blue', 'res/projectiles/bullet_round_blue.png');
-	game.load.image('bullet_round_purple', 'res/projectiles/bullet_round_purple.png');
-	game.load.image('bullet_round_yellow', 'res/projectiles/bullet_round_yellow.png');
-	
-	// visual effects
-	game.load.spritesheet('explosion_small', 'res/effects/explosion_small.png', 16, 16);
-	
-	// scripts
-	game.load.text('level_1', 'data/scripts/level_1.script');
-	game.load.json('enemy', 'data/enemy.json');
-	game.load.json('projectile', 'data/projectile.json');
-	game.load.json('projectilePattern', 'data/projectilePatterns.json');
 }
 
 function create () {
-	enemyData = game.cache.getJSON('enemy').enemyData;
-	projectileData = game.cache.getJSON('projectile').projectileData;
-	projectilePatternData = game.cache.getJSON('projectilePattern').patternData;
-	var levelScriptData = game.cache.getText('level_1');
+    graphics = game.add.graphics(0, 0);
+	grid = new Array(GAME_WIDTH);	// height
+	for( var i = 0; i < GAME_WIDTH; i++) {
+		grid[i] = new Array(GAME_HEIGHT);	// width
+	}
 	
-	enemies = game.add.group();
-	enemies.enableBody = true;
-	enemies.physicsBodyType = Phaser.Physics.ARCADE;
-		
-	enemyProjectileGroup = game.add.group();
-	enemyProjectileGroup.enableBody = true;
-	enemyProjectileGroup.physicsBodyType = Phaser.Physics.ARCADE;
+	for( var i = 0; i < GAME_WIDTH; i++ ) {
+		for( var j = 0; j < GAME_HEIGHT; j++ ) {
+			grid[i][j] = 0;
+		}
+	}
 	
-	loadScript(levelScriptData);
-	playerCreate();
+	spawnFood();
+	
+	playerX = 15;
+	playerY = 17;
+	updateDelay = 10;
 	
 	input = game.input;
 	game.input.mouse.capture = true;
-	clickLeft = game.input.activePointer.leftButton;
-	clickRight = game.input.activePointer.rightButton;
-	buttonShoot = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    window.graphics = graphics;
 }
 
 function render() {
+	graphics.clear();
+	for( var i = 0; i < GAME_WIDTH; i++ ) {
+		for( var j = 0; j < GAME_HEIGHT; j++ ) { 
+			var cellData = grid[i][j];
+			
+			if( cellData == "F") {
+				graphics.beginFill("0x00FF00");
+				graphics.drawRect( (i * CELL_SIZE) ,  (j * CELL_SIZE), CELL_SIZE, CELL_SIZE);
+				graphics.endFill();
+			}else if( cellData == 0) {
+				graphics.beginFill("0x000000");
+				graphics.drawRect( (i * CELL_SIZE) ,  (j * CELL_SIZE), CELL_SIZE, CELL_SIZE);
+				graphics.endFill();
+			}else if( cellData > 0) {
+				var decay = ( cellData / (score + 5)) * 100;
+				var val;
+				if( decay <= 10 ) {
+					val = "0x111111";
+				}else if( decay <= 20) {
+					val = "0x444444";
+				}else if( decay <= 30) {
+					val = "0x777777";
+				}else if( decay <= 40) {
+					val = "0xAAAAAA";
+				}else if( decay <= 50) {
+					val = "0xDDDDDD";
+				}else {
+					val = "0xFFFFFF";
+				}
+				graphics.beginFill(val);
+				graphics.drawRect( (i * CELL_SIZE) ,  (j * CELL_SIZE), CELL_SIZE, CELL_SIZE);
+				graphics.endFill();
+			}
+		}
+	}
+}
+
+function spawnFood() {
+	var newX = game.math.roundTo(game.math.between(5, GAME_WIDTH - 5), 0);
+	var newY = game.math.roundTo(game.math.between(5, GAME_HEIGHT - 5), 0);
+	grid[newX][newY]  = ENTITY_FOOD;
+}
+
+function playerUpdate() {
+	for( var i = 0; i < GAME_WIDTH; i++ ) {
+		for( var j = 0; j < GAME_HEIGHT; j++ ) {
+			if( grid[i][j] > 0 ) {
+				grid[i][j]--;
+			}
+		}
+	}
+	grid[playerX][playerY] = (score + 10);
 	
+	if(playerDirection == DIRECTION_UP) {
+		playerY--;
+		
+		if(playerY == -1) {
+			playerY = GAME_HEIGHT - 1;
+		}
+	}
+	
+	if(playerDirection == DIRECTION_DOWN) {
+		playerY++;
+		
+		if(playerY == GAME_HEIGHT) {
+			playerY = 0;
+		}
+	}
+	
+	if(playerDirection == DIRECTION_LEFT) {
+		playerX--;
+		
+		if(playerX == -1) {
+			playerX = GAME_WIDTH - 1;
+		}
+	}
+	
+	if(playerDirection == DIRECTION_RIGHT) {
+		playerX++;
+		
+		if(playerX == GAME_WIDTH) {
+			playerX = 0;
+		}
+	}
+	
+	if(grid[playerX][playerY] == "F") {
+		score += 2;
+		spawnFood();
+	}
+}
+
+function handleInput() {
+	var key = input.keyboard;
+	if ( key.isDown(Phaser.KeyCode.LEFT) || key.isDown(Phaser.KeyCode.A)) {
+		playerDirection = DIRECTION_LEFT;
+	}
+	if (key.isDown(Phaser.KeyCode.RIGHT) || key.isDown(Phaser.KeyCode.D) ) {
+		playerDirection = DIRECTION_RIGHT;
+	}
+	if (key.isDown(Phaser.KeyCode.UP) || key.isDown(Phaser.KeyCode.W) ) {
+		playerDirection = DIRECTION_UP;
+	}
+	if (key.isDown(Phaser.KeyCode.DOWN) || key.isDown(Phaser.KeyCode.S) ) {
+		playerDirection = DIRECTION_DOWN;
+	}
 }
 
 function update() {
-	if (player.alive) {
-		//  Reset the player, then check for movement keys
-		player.body.velocity.setTo(0, 0);
-		var key = input.keyboard;
-		if ( key.isDown(Phaser.KeyCode.LEFT) || key.isDown(Phaser.KeyCode.A)) {
-			player.body.velocity.x = -playerSpeed;
-		}
-		if (key.isDown(Phaser.KeyCode.RIGHT) || key.isDown(Phaser.KeyCode.D) ) {
-			player.body.velocity.x = playerSpeed;
-		}
-		if (key.isDown(Phaser.KeyCode.UP) || key.isDown(Phaser.KeyCode.W) ) {
-			player.body.velocity.y = -playerSpeed;
-		}
-		if (key.isDown(Phaser.KeyCode.DOWN) || key.isDown(Phaser.KeyCode.S) ) {
-			player.body.velocity.y = playerSpeed;
-		}
-		if ( clickLeft.isDown || buttonShoot.isDown ) { 
-			playerShoot();
-		}
-	}
-	
-	if(shootDelay > 0) {
-		shootDelay--;
-	}
-	
-	enemies.forEachAlive(entityWaypointUpdate, this);
-	enemyProjectileGroup.forEachAlive(entityWaypointUpdate, this);
-	game.physics.arcade.overlap(bullets, enemies, bulletEnemyCollisionHandler, null, this);
+	handleInput();
+	playerUpdate();
 }
 
