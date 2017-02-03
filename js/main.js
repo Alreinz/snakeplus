7bdb8482d8b20e558b11f4cc83c6c882d1383dcf
Albert <lorenzdiz@gmail.com>
2017-02-03 23:18:57 +0800

    added speed controls (o - increase p - decrease) added graphical effect at the end of the snake added ui elements - score - speed changed snake length to unique variable instead of using score changed game dimensions to 1000x800 from 800x600

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 window.onload = function() {
-	game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, render: render, update: update});
+	game = new Phaser.Game(1000, 800, Phaser.AUTO, '', { preload: preload, create: create, render: render, update: update});
 	math = game.math;
 }
 
@@ -10,6 +10,8 @@ var graphics;
 var CELL_SIZE = 8;
 var GAME_WIDTH = 100;
 var GAME_HEIGHT = 75;
+var OFFSET_WIDTH = 100;
+var OFFSET_HEIGHT = 100;
 
 // Game States
 var gameState;
@@ -32,9 +34,8 @@ var playerY;
 
 // Player properties
 var score = 0;
+var snakeLength = 10;
 var health = 10;
-var playerSpeed = 250;
-var playerAcceleration = 100;
 
 var playerDirection = 0;
 var DIRECTION_UP = 0;
@@ -42,14 +43,24 @@ var DIRECTION_DOWN = 1;
 var DIRECTION_LEFT = 2;
 var DIRECTION_RIGHT = 3;
 
+// game delay
+var speed = 0;
 var updateDelay;
 var grid;
+
+// UI Elements
+var textScore;
+var textSpeed;
+
+// Keys
+var buttonSpeedUp;
+var buttonSpeedDown;
 
 function preload () {
 }
 
 function create () {
-    graphics = game.add.graphics(0, 0);
+	graphics = game.add.graphics(0, 0);
 	grid = new Array(GAME_WIDTH);	// height
 	for( var i = 0; i < GAME_WIDTH; i++) {
 		grid[i] = new Array(GAME_HEIGHT);	// width
@@ -65,55 +76,102 @@ function create () {
 	
 	playerX = 15;
 	playerY = 17;
-	updateDelay = 10;
+	updateDelay = speed;
 	
 	input = game.input;
 	game.input.mouse.capture = true;
     window.graphics = graphics;
+	
+	buttonSpeedUp = game.input.keyboard.addKey(Phaser.Keyboard.O);
+	buttonSpeedUp.onDown.add(speedUp, this);
+	buttonSpeedDown = game.input.keyboard.addKey(Phaser.Keyboard.P);
+	buttonSpeedDown.onDown.add(speedDown, this);
+	
+	var style = { font: "15px Courier", fill: "#FFF" };
+	textScore = game.add.text(100, 85, "Score: 0", style);
+	textSpeed = game.add.text(300, 85, "Speed: 10", style);
 }
 
 function render() {
 	graphics.clear();
+	
+	graphics.lineStyle(2,  "0xFFFFFF", 100);
+	graphics.drawRect( OFFSET_WIDTH,  OFFSET_HEIGHT, 800, 600);
+	graphics.lineStyle(0,  "0xFFFFFF", 100);
+	
 	for( var i = 0; i < GAME_WIDTH; i++ ) {
 		for( var j = 0; j < GAME_HEIGHT; j++ ) { 
 			var cellData = grid[i][j];
 			
 			if( cellData == "F") {
 				graphics.beginFill("0x00FF00");
-				graphics.drawRect( (i * CELL_SIZE) ,  (j * CELL_SIZE), CELL_SIZE, CELL_SIZE);
+				graphics.drawRect( OFFSET_WIDTH + (i * CELL_SIZE) , OFFSET_HEIGHT + (j * CELL_SIZE), CELL_SIZE, CELL_SIZE);
 				graphics.endFill();
 			}else if( cellData == 0) {
 				graphics.beginFill("0x000000");
-				graphics.drawRect( (i * CELL_SIZE) ,  (j * CELL_SIZE), CELL_SIZE, CELL_SIZE);
+				graphics.drawRect( OFFSET_WIDTH + (i * CELL_SIZE) , OFFSET_HEIGHT + (j * CELL_SIZE), CELL_SIZE, CELL_SIZE);
 				graphics.endFill();
 			}else if( cellData > 0) {
-				var decay = ( cellData / (score + 5)) * 100;
+				var decay = ( cellData / (snakeLength) ) * 100;
+				console.log(decay);
 				var val;
-				if( decay <= 10 ) {
+				if( decay <= 1) {
 					val = "0x111111";
-				}else if( decay <= 20) {
-					val = "0x444444";
-				}else if( decay <= 30) {
+				}else if( decay <= 2) {
+					val = "0x333333";
+				}else if( decay <= 3) {
+					val = "0x555555";
+				}else if( decay <= 4) {
 					val = "0x777777";
-				}else if( decay <= 40) {
+				}else if( decay <= 5) {
+					val = "0x999999";
+				}else if( decay <= 6) {
 					val = "0xAAAAAA";
-				}else if( decay <= 50) {
+				}else if( decay <= 7) {
+					val = "0xBBBBBB";
+				}else if( decay <= 8) {
+					val = "0xCCCCCC";
+				}else if( decay <= 9) {
 					val = "0xDDDDDD";
+				}else if( decay <= 10) {
+					val = "0xEEEEEE";
 				}else {
 					val = "0xFFFFFF";
 				}
 				graphics.beginFill(val);
-				graphics.drawRect( (i * CELL_SIZE) ,  (j * CELL_SIZE), CELL_SIZE, CELL_SIZE);
+				graphics.drawRect( OFFSET_WIDTH +  (i * CELL_SIZE) ,  OFFSET_HEIGHT +  (j * CELL_SIZE), CELL_SIZE, CELL_SIZE);
 				graphics.endFill();
 			}
 		}
 	}
 }
 
+function updateTextScore() {
+	textScore.setText("Score: " + score);
+}
+
+function updateTextSpeed() {
+	textSpeed.setText("Speed: " + (10 - speed) );
+}
+
 function spawnFood() {
 	var newX = game.math.roundTo(game.math.between(5, GAME_WIDTH - 5), 0);
 	var newY = game.math.roundTo(game.math.between(5, GAME_HEIGHT - 5), 0);
 	grid[newX][newY]  = ENTITY_FOOD;
+}
+
+function speedUp() {
+	if( speed < 9 ) {
+		speed++;
+		updateTextSpeed();
+	}
+}
+
+function speedDown() {
+	if( speed > 0) {
+		speed--;
+		updateTextSpeed();
+	}
 }
 
 function playerUpdate() {
@@ -124,7 +182,7 @@ function playerUpdate() {
 			}
 		}
 	}
-	grid[playerX][playerY] = (score + 10);
+	grid[playerX][playerY] = (snakeLength);
 	
 	if(playerDirection == DIRECTION_UP) {
 		playerY--;
@@ -159,29 +217,40 @@ function playerUpdate() {
 	}
 	
 	if(grid[playerX][playerY] == "F") {
-		score += 2;
+		score += 10 - speed;
+		snakeLength += 5;
 		spawnFood();
+		updateTextScore();
+	}else if(grid[playerX][playerY] > 0) {
+		score -= ((10 - speed) / 2);
+		updateTextScore();
 	}
 }
 
 function handleInput() {
 	var key = input.keyboard;
-	if ( key.isDown(Phaser.KeyCode.LEFT) || key.isDown(Phaser.KeyCode.A)) {
+	if ( (key.isDown(Phaser.KeyCode.LEFT) || key.isDown(Phaser.KeyCode.A)) && playerDirection != DIRECTION_RIGHT) {
 		playerDirection = DIRECTION_LEFT;
 	}
-	if (key.isDown(Phaser.KeyCode.RIGHT) || key.isDown(Phaser.KeyCode.D) ) {
+	if ( (key.isDown(Phaser.KeyCode.RIGHT) || key.isDown(Phaser.KeyCode.D)) && playerDirection != DIRECTION_LEFT) {
 		playerDirection = DIRECTION_RIGHT;
 	}
-	if (key.isDown(Phaser.KeyCode.UP) || key.isDown(Phaser.KeyCode.W) ) {
+	if ( (key.isDown(Phaser.KeyCode.UP) || key.isDown(Phaser.KeyCode.W)) && playerDirection != DIRECTION_DOWN ) {
 		playerDirection = DIRECTION_UP;
 	}
-	if (key.isDown(Phaser.KeyCode.DOWN) || key.isDown(Phaser.KeyCode.S) ) {
+	if ( (key.isDown(Phaser.KeyCode.DOWN) || key.isDown(Phaser.KeyCode.S)) && playerDirection != DIRECTION_UP) {
 		playerDirection = DIRECTION_DOWN;
 	}
 }
 
 function update() {
 	handleInput();
-	playerUpdate();
+	
+	if( updateDelay == 0) {
+		playerUpdate();
+		updateDelay = speed;
+	}else {
+		updateDelay--;
+	}
 }
 
